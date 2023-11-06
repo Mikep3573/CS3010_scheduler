cd5bac2d5f590402ab43515feca36342477fe249
Mikep3573 <m.piscione3456@gmail.com>
2023-11-06 11:07:01 -0500

    added Jason's priority queue implementations

---
+++ pqueue.mpiscion.h
@@ -0,0 +1,24 @@
+// Jason's priority queue header file
+
+#define MAX_NAME_LENGTH 63
+
+typedef struct {
+  unsigned int weight;
+  char name[1 + MAX_NAME_LENGTH];
+} Thing;
+
+typedef struct PQueueNodeStruct {
+  int priority;
+  void *data;
+  struct PQueueNodeStruct *next;
+} PQueueNode;
+
+int enqueue(PQueueNode **pqueue, int priority, void *data);
+void *dequeue(PQueueNode **pqueue);
+void *peek(PQueueNode *pqueue);
+void printQueue(PQueueNode *pqueue, void (printFunction)(void*));
+int getMinPriority(PQueueNode *pqueue);
+int queueLength(PQueueNode *pqueue);
+
+void printThing(void *data);
+
